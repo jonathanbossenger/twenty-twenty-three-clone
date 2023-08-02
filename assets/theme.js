565aecd5e373d2781f31828cc9e3d69b1c5c1767
Jonathan Bossenger <jonathanbossenger@gmail.com>
2023-08-02 17:34:49 +0200

    Translation ready

--- assets/theme.js
+++ assets/theme.js
@@ -2,6 +2,7 @@
  * Add event listener to the button
  */
 document.querySelector( '#twentytwentythreeclone-settings-button' ).addEventListener( 'click', settingsButtonClick );
+
 function settingsButtonClick() {
-	alert( 'Settings button clicked' );
+	alert( __( 'Settings button clicked', 'twentytwentythreeclone' ) );
 }
--- functions.php
+++ functions.php
@@ -5,13 +5,14 @@
  */
 add_action( 'admin_enqueue_scripts', 'twentytwentythreeclone_enqueue_scripts' );
 function twentytwentythreeclone_enqueue_scripts() {
-    wp_enqueue_script(
-        'twentytwentythreeclone-theme-js',
-        get_stylesheet_directory_uri() . '/assets/theme.js',
-        array(),
-        '1.0.0',
-        true
-    );
+	wp_enqueue_script(
+		'twentytwentythreeclone-theme-js',
+		get_stylesheet_directory_uri() . '/assets/theme.js',
+		array( 'wp-i18n' ),
+		'1.0.0',
+		true
+	);
+	wp_set_script_translations('twentytwentythreeclone-theme-js', 'twentytwentythreeclone');
 }
 
 /**
@@ -21,8 +22,8 @@ add_action( 'admin_menu', 'twentytwentythreeclone_add_submenu_page' );
 function twentytwentythreeclone_add_submenu_page() {
 	add_submenu_page(
 		'themes.php', // parent slug
-		'Twenty Twenty Three Clone', // page title
-		'Twenty Twenty Three Clone', // menu title
+		__( 'Twenty Twenty Three Clone', 'twentytwentythreeclone' ), // page title
+		__( 'Twenty Twenty Three Clone', 'twentytwentythreeclone' ), // menu title
 		'manage_options', // capability
 		'twentytwentythreeclone', // slug
 		'twentytwentythreeclone_display_page' // callback
@@ -34,11 +35,11 @@ function twentytwentythreeclone_add_submenu_page() {
  */
 function twentytwentythreeclone_display_page() {
 	?>
-	<div class="wrap">
-		<h1>Twenty Twenty Three Clone Settings</h1>
-		<p>This is a settings page for the Twenty Twenty Three Clone theme</p>
-        <button id="twentytwentythreeclone-settings-button" class="button button-primary">Alert</button>
-	</div>
+    <div class="wrap">
+        <h1><?php _e( 'Twenty Twenty Three Clone Settings', 'twentytwentythreeclone' ); ?></h1>
+        <p><?php _e( 'This is a settings page for the Twenty Twenty Three Clone theme', '__' ); ?></p>
+        <button id="twentytwentythreeclone-settings-button" class="button button-primary"><?php _e( 'Alert', 'twentytwentythreeclone' ); ?></button>
+    </div>
 	<?php
 }
 
